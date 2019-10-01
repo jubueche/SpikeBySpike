a6d7d2963475e59c11aa9ccfbae217e1894bda95
Dmitry Zendrikov <dmitrii@ini.uzh.ch>
2019-10-01 21:53:21 +0200

    Add application of the feedforward matrix to the chip

--- DYNAPS/playground.py
+++ DYNAPS/playground.py
@@ -17,10 +17,10 @@ print(self_path)
 sbs.c.execute("exec(open('" + self_path + "/Resources/dynapse_biases.py').read())")
 
 # Connect 4 virtual neurons to 4 real neurons
-sbs.connector.add_connection(sbs.v_neurons[1], sbs.neurons[1 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
-sbs.connector.add_connection(sbs.v_neurons[2], sbs.neurons[2 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
-sbs.connector.add_connection(sbs.v_neurons[3], sbs.neurons[3 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
-sbs.connector.add_connection(sbs.v_neurons[4], sbs.neurons[4 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
+#sbs.connector.add_connection(sbs.v_neurons[1], sbs.neurons[1 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
+#sbs.connector.add_connection(sbs.v_neurons[2], sbs.neurons[2 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
+#sbs.connector.add_connection(sbs.v_neurons[3], sbs.neurons[3 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
+#sbs.connector.add_connection(sbs.v_neurons[4], sbs.neurons[4 + 256 *(sbs.core_id % 4) + 1024*sbs.chip_id], sbs.SynTypes.SLOW_EXC)
 
 sbs.model.apply_diff_state()
 
