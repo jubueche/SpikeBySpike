f1fb38d91e3a7dc4dcebfab1f34854300bcfe440
Julian Buechel <julianb@ini.uzh.ch>
2019-10-02 20:38:09 +0200

    WIP for Sumatra

--- runnet.py
+++ runnet.py
@@ -1,14 +1,25 @@
 import numpy as np  
 from Utils import my_max
+import json
+import os
+
 
 def runnet(dt, lam, F, Input, C, Nneuron, Ntime, Thresh):
+
+    with open(os.path.join(os.getcwd(), "/parameters.param"), 'r') as f:
+            parameters = json.load(f)
+
     r0 = np.zeros((Nneuron, Ntime))
     O = np.zeros((Nneuron, Ntime))
     V = np.zeros((Nneuron, Ntime))
-
+    I = np.zeros((Nneuron, Ntime))
+    R = parameters["R"]
 
     for t in range(1, Ntime):
-        V[:,t] = ((1-lam*dt)*V[:,t-1].reshape((-1,1)) + dt*np.matmul(F.T, Input[:,t-1].reshape((-1,1))) + np.matmul(C, O[:,t-1].reshape((-1,1))) + 0.001*np.random.randn(Nneuron,1)).ravel()
+        #! julianb
+        I[:,t] = ((1-R*dt)*I[:,t-1].reshape((-1,1)) + dt*np.matmul(F.T, Input[:,t-1].reshape((-1,1))) + np.matmul(C, O[:,t-1].reshape((-1,1))) + 0.001*np.random.randn(Nneuron,1)).ravel()
+        V[:,t] = (1-dt)*V[:,t-1] + dt*R*I
+        #V[:,t] = ((1-lam*dt)*V[:,t-1].reshape((-1,1)) + dt*np.matmul(F.T, Input[:,t-1].reshape((-1,1))) + np.matmul(C, O[:,t-1].reshape((-1,1))) + 0.001*np.random.randn(Nneuron,1)).ravel()
         (m,k) = my_max(V[:,t].reshape((-1,1)) - Thresh-0.01*np.random.randn(Nneuron, 1))
 
         if(m>=0):
